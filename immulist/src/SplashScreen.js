426ce3a9e5e7f3ed5986973b276b2eab6aa40f25
jadhlady <56136691+jadhlady@users.noreply.github.com>
2020-01-11 19:09:07 -0800

    Add files via upload

--- immulist/src/SplashScreen.js
+++ immulist/src/SplashScreen.js
@@ -6,7 +6,7 @@ export default function SplashInfo() {
     <div className="Logo">
       <h1>Immunization App</h1>
       <div classname="AppImage">
-      <img src="https://image.flaticon.com/icons/svg/1086/1086932.svg" width="75" height="75"> 
+      <img src="https://image.flaticon.com/icons/svg/1086/1086932.svg" width="75" height="75" alt="">
       </img>
       </div>
       <h2>Placeholder</h2>
