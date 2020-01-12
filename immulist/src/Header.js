480d51405c6f1d00a5a6ebf104f7ea1df30239a6
michaux <michaux2817@outlook.com>
2020-01-11 22:07:54 -0800

    Working on Global Nav

--- immulist/src/Header.js
+++ immulist/src/Header.js
@@ -7,20 +7,32 @@ class Header extends React.Component{
         return (
             <header>
                 <div className="logo">
-                    <h1>Immunization App</h1>
-                    <img id='headerPicture' src="https://image.flaticon.com/icons/svg/1086/1086932.svg"/>
+                    
                     <nav>
-                    <NavLink exact activeClassName="active" to="/dashboard">
-                        Dashboard
-                    </NavLink>
-                    <NavLink activeClassName="active" to="/account">
-                        Account
-                    </NavLink>
-                    <NavLink activeClassName="active" to="/">
-                        Sign out
-                    </NavLink>
+                        <div className="navLogo">
+                            <h1>Immunization App</h1>
+                            <img id='headerPicture' src="https://image.flaticon.com/icons/svg/1086/1086932.svg"/>
+                        </div>
+                        
+                        <div className="titles">
+                            <NavLink exact activeClassName="active" to="/dashboard">
+                                Dashboard
+                            </NavLink>
+                        </div>
+                        <div className="titles">
+                            <NavLink activeClassName="active" to="/account">
+                                Account
+                            </NavLink>
+                        </div>
+                        <div className="titles">
+                            <NavLink activeClassName="active" to="/">
+                                Sign out
+                            </NavLink>
+                        </div>
                     </nav>
                 </div>
+                
+                
             </header>
         );
     }
