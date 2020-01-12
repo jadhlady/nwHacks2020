5ecf7b6f8ad324f847b534d3d7f2a27e82d3d3a0
joyce <joyce.cxc@gmail.com>
2020-01-11 19:30:56 -0800

    adding header

--- immulist/src/index.js
+++ immulist/src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from './App';
 
 import SplashInfo from "./SplashScreen";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<SplashInfo />, rootElement);
+ReactDOM.render(<App />, rootElement);
