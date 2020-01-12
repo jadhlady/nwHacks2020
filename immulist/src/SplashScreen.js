ef88cfb471a62e8ce0cfa8daebe2ea1b2e061a3b
jadhlady <56136691+jadhlady@users.noreply.github.com>
2020-01-11 16:44:03 -0800

    Update and rename App.js to SplashScreen.js

---
+++ immulist/src/SplashScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function SplashInfo() {
+  return (
+    <div className="Logo">
+      <h1>Immunization App</h1>
+      <div classname="AppImage">
+      <img src="https://image.flaticon.com/icons/svg/1086/1086932.svg" width="75" height="75"> 
+      </img>
+      </div>
+      <h2>Placeholder</h2>
+    </div>
+  );
+}
