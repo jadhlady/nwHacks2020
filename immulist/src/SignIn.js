b0d5dd3f600b52a6579ed270a243bffc428b9f88
Curtis Koo <ckksales1@gmail.com>
2020-01-12 10:29:57 -0800

    add sign in page

---
+++ immulist/src/SignIn.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import { Redirect } from 'react-router-dom'
+
+const styles = {
+    button: {
+        // padding: "10px",
+        margin: "10px"
+    },
+    input: {
+        display: 'none',
+    },
+    card: {
+        margin: "20px",
+        width: "70%",
+        textAlign: "centre",
+    }
+};
+
+
+class SignIn extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userType: null
+        }
+    }
+
+    userTypeHandler(type) {
+        this.setState({userType: type})
+    }
+
+    renderRedirect = () => {
+        this.context.router.push({
+            pathname: '/confirmation',
+            state: {email: this.state.email}
+        })
+        return <Redirect to='/'/>
+    }
+
+    render(){
+        return (
+            <div>
+                {!this.state.userType
+                    ?
+                    <Card style={styles.card}>
+                        <CardHeader
+                            title="What type of user are you?"
+                        />
+                        <Button variant="contained" color="primary" style={styles.button} onClick={() => {
+                            this.userTypeHandler("client")
+                        }}>
+                            Client
+                        </Button>
+                        <Button variant="contained" color="primary" style={styles.button} onClick={() => {
+                            this.userTypeHandler("clinic")
+                        }}>
+                            Clinic
+                        </Button>
+                    </Card>
+                    :
+                    <div>
+                        {this.renderRedirect()}
+                    </div>
+
+                }
+            </div>
+        );
+    }
+}
+
+export default SignIn;
