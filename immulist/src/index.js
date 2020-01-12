8d9a6da44f26af8fc5139aaae415956cf1be59e1
jadhlady <56136691+jadhlady@users.noreply.github.com>
2020-01-11 19:46:54 -0800

    Add files via upload

--- immulist/src/index.js
+++ immulist/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App';
 
 import SplashInfo from "./SplashScreen";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<SplashInfo />, rootElement);
